a06940ecdfcd615f7384a98bb3d9a97fd720ab4b
ytqxwt <3478390471@qq.com>
2018-06-02 14:17:56 +0800

    Initial commit

---
+++ src/main/java/xin/geek/hackson/HacksonApplication.java
@@ -0,0 +1,12 @@
+package xin.geek.hackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacksonApplication.class, args);
+    }
+}
